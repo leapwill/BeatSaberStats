eeeb5e8fac7b9092daade25c4a07c2671b29fc9d
leapwill <leapwill@users.noreply.github.com>
2021-06-01 22:33:57 -0500

    Drastically rein in memory usage

--- Search-BeatSaberData.ps1
+++ Search-BeatSaberData.ps1
@@ -7,6 +7,7 @@ The path of the directory containing Beat Saber.exe
 The 0-indexed number of player data to use in PlayerData.dat
 #>
 
+[CmdletBinding()]
 param(
     [string]
     $SavePath = '',
@@ -59,15 +60,43 @@ if (-not ($PlayerData.Length -ge $PlayerNumber + 1)) {
 $PlayerData = $PlayerData[$PlayerNumber]
 #endregion
 
+#region functions to keep scopes small for memory
+Add-Type -AssemblyName 'System.Security.Cryptography.Primitives'
+# calculate hash to find level ID in save file
+function Load-HashedJson {
+    param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [System.Security.Cryptography.SHA1]
+        $Hasher,
+        [Parameter(Mandatory=$true, Position=1)]
+        [string]
+        $Path,
+        [Parameter(Position=2)]
+        [switch]
+        $IsFinal
+    )
+    $fileRaw = Get-Content $Path -Raw
+    $fileBytes = [System.Text.Encoding]::UTF8.GetBytes($fileRaw)
+    if ($IsFinal) {
+        $Hasher.TransformFinalBlock($fileBytes, 0, $fileBytes.Length) >$null
+    }
+    else {
+        $Hasher.TransformBlock($fileBytes, 0, $fileBytes.Length, $null, 0) >$null
+    }
+    return ConvertFrom-Json $fileRaw
+}
+#endregion
+
+# TODO threading, probably C# for PS5.0 compat, with a semaphore
 #(Get-CIMInstance -Class 'CIM_Processor').NumberOfLogicalProcessors
 
-$DifficultyRankMap = @{
-    1 = 'Y';
-    3 = 'N';
-    5 = 'H';
-    7 = 'E';
-    9 = 'E+'
-}
+$DifficultyRankMap = @(
+    'e',
+    'N',
+    'H',
+    'E',
+    'E+'
+)
 $ScoreRankMap = @(
     'E',
     'D',
@@ -84,9 +113,15 @@ $ScoreRankMap = @(
 $CustomLevelsPath = Join-Path $LevelsPath 'CustomLevels'
 $CustomLevelInfoFiles = Get-ChildItem $CustomLevelsPath -Recurse -Filter 'info.dat'
 $LevelStats = New-Object 'System.Collections.Generic.List[object]' -ArgumentList $CustomLevelInfoFiles.Length
-Add-Type -AssemblyName 'System.Security.Cryptography.Primitives'
+$Stopwatch = New-Object System.Diagnostics.Stopwatch
 foreach ($levelInfoFile in $CustomLevelInfoFiles) {
-    $levelInfoSrc = ConvertFrom-Json (Get-Content $levelInfoFile.FullName -Raw)
+    Write-Verbose "processing $($levelInfoFile.Directory.Name)"
+    $Stopwatch.Restart()
+
+    $hasher = [System.Security.Cryptography.SHA1]::Create()
+
+    $levelInfoSrc = Load-HashedJson $hasher $levelInfoFile.FullName
+
     $levelInfo = @{
         'Song' = $levelInfoSrc._songName;
         'Artist' = $levelInfoSrc._songAuthorName;
@@ -94,23 +129,26 @@ foreach ($levelInfoFile in $CustomLevelInfoFiles) {
         'BPM' = $levelInfoSrc._beatsPerMinute;
         'Envrionment' = $levelInfoSrc._environmentName;
     }
-    <# setup object to be the same every level
-    foreach ($prefix in $DifficultyRankMap.Values) {
+    # setup object to be the same every level
+    foreach ($prefix in $DifficultyRankMap) {
         $levelInfo["$prefix Valid"] = $levelInfo["$prefix Plays"] = $levelInfo["$prefix Rank"] = $levelInfo["$prefix Combo"] = $levelInfo["$prefix Score"] = $levelInfo["$prefix NP10S"] = $levelInfo["$prefix ~Duration"] = $levelInfo["$prefix ~NPS"] = $levelInfo["$prefix Notes"] = ''
-    }#>
-
-    $bytesToHash = New-Object 'System.Collections.Generic.List[byte]'
-    # TODO use C# HashAlgorithm#TransformBlock to read files in chunks for less memory?
-    $bytesToHash.AddRange([byte[]](Get-Content $levelInfoFile.FullName -AsByteStream 3>$null))
+    }
 
     # TODO one-hand/90/360/lightshow _difficultyBeatmapSets
-    $standardMaps = ($levelInfoSrc._difficultyBeatmapSets | Where-Object { $_._beatmapCharacteristicName -eq 'Standard' })._difficultyBeatmaps
+    $standardMaps = ($levelInfoSrc._difficultyBeatmapSets | Where-Object { $_._beatmapCharacteristicName -eq 'Standard' })
+    if ($standardMaps -eq $null -or $standardMaps.Length -eq 0) {
+        Write-Information "No Standard beatmaps found, skipping $($levelInfoFile.Directory.Name)"
+        continue
+    }
+    else {
+        $standardMaps = $standardMaps._difficultyBeatmaps
+    }
+    Write-Debug "info done at `t$($Stopwatch.ElapsedMilliseconds)"
     foreach ($difficultyInfo in $standardMaps) {
-        $prefix = $DifficultyRankMap[$difficultyInfo._difficultyRank]
-        $bytesToHash.AddRange([byte[]](Get-Content (Join-Path $levelInfoFile.DirectoryName $difficultyInfo._beatmapFilename) -AsByteStream 3>$null))
+        $prefix = $DifficultyRankMap[[Math]::Floor($difficultyInfo._difficultyRank / 2)]
 
         # read beatmap and calc stats
-        $beatmapNotes = (Get-Content (Join-Path $levelInfoFile.DirectoryName $difficultyInfo._beatmapFilename) -Raw | ConvertFrom-Json)._notes
+        $beatmapNotes = (Load-HashedJson $hasher (Join-Path $levelInfoFile.DirectoryName $difficultyInfo._beatmapFilename) $($difficultyInfo -eq $standardMaps[$standardMaps.Length - 1]))._notes
         $levelInfo["$prefix Notes"] = $beatmapNotes.Length
         # TODO for real NPS, song length comes from reading _songFilename. need ffmpeg?
         $firstNoteTime = $beatmapNotes[0]._time
@@ -136,16 +174,15 @@ foreach ($levelInfoFile in $CustomLevelInfoFiles) {
         
 
     }
+    Write-Debug "difficulties done at $($Stopwatch.ElapsedMilliseconds)"
 
-    # calculate hash to find level ID in save file
-    [System.Security.Cryptography.HashAlgorithm]$hash = [System.Security.Cryptography.SHA1]::Create()
-    $hash.ComputeHash($bytesToHash) >$null
-    $hashStr = [System.BitConverter]::ToString($hash.Hash) -Replace '-',''
+    # read save file for scores and stuff
+    $hashStr = [System.BitConverter]::ToString($hasher.Hash) -Replace '-',''
     $levelId = "custom_level_$hashStr"
     $levelInfo['ID'] = $levelId
     $scores = $PlayerData.levelsStatsData | Where-Object {$_.levelId -iLike $levelId -and $_.beatmapCharacteristicName -eq 'Standard'}
     foreach ($score in $scores) {
-        $prefix = $DifficultyRankMap[$score.difficulty]
+        $prefix = $DifficultyRankMap[[Math]::Floor($score.difficulty / 2)]
         $levelInfo["$prefix Score"] = $score.highScore
         if ($score.fullCombo) {
             $levelInfo["$prefix Combo"] = 'FC'
@@ -157,9 +194,15 @@ foreach ($levelInfoFile in $CustomLevelInfoFiles) {
         $levelInfo["$prefix Plays"] = $score.playCount
         $levelInfo["$prefix Valid"] = $score.validScore
     }
+    Write-Debug "scores done at `t$($Stopwatch.ElapsedMilliseconds)"
 
     $LevelStats.Add($levelInfo)
-    Write-Host "processed $levelId"
+    Write-Verbose "processed $levelId"
 }
 
-Export-Csv $LevelStats -Path 'stats.csv'
+# TODO use -Append to avoid storing all in memory until the end?
+# TODO get the columns in a sensible order
+Remove-Item 'stats.csv'
+foreach ($lvl in $LevelStats) {
+    Export-Csv -InputObject ([pscustomobject]$lvl) -Append -Path 'stats.csv'
+}
