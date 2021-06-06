98f58323e57793f299d96e8404a9449b61d7e132
leapwill <leapwill@users.noreply.github.com>
2021-06-05 21:01:46 -0500

    Fixes including correct note units

--- Search-BeatSaberData.ps1
+++ Search-BeatSaberData.ps1
@@ -7,7 +7,8 @@ The path of the directory containing Beat Saber.exe
 The 0-indexed number of player data to use in PlayerData.dat
 #>
 
-#Requires -Version 5
+# TODO get down to 5, blocker is System.Security.Cryptography.Primitives
+#Requires -Version 6
 
 [CmdletBinding()]
 param(
@@ -22,7 +23,7 @@ param(
 Set-StrictMode -Version Latest
 
 #region parameter setup
-$IsWin = $IsWindows -or $Env:OS -Match 'Windows'
+$IsWin = $Env:OS -Match 'Windows'
 
 if ($SavePath -eq '') {
     if ($IsWin) {
@@ -57,7 +58,7 @@ if (-not (Test-Path $LevelsPath -PathType Container)) {
 $PlayerData = (Get-Content $SavePath | ConvertFrom-Json).LocalPlayers
 if (-not ($PlayerData.Length -ge $PlayerNumber + 1)) {
     Write-Error 'No players found in the save file'
-    exit -3
+    exit -4
 }
 $PlayerData = $PlayerData[$PlayerNumber]
 #endregion
@@ -89,11 +90,11 @@ function Load-HashedJson {
 }
 #endregion
 
-# TODO threading, probably C# for PS5.0 compat, with a semaphore
-#(Get-CIMInstance -Class 'CIM_Processor').NumberOfLogicalProcessors
+# TODO threading, probably C# for PS5.0 compat. with a semaphore? free mem check (0.5GB per thread)? currently ~1.5s and 100-600MB per song
+#(Get-CIMInstance -Class CIM_Processor).NumberOfLogicalProcessors
 
 $DifficultyRankMap = @(
-    'e',
+    'Y',
     'N',
     'H',
     'E',
@@ -116,6 +117,7 @@ $CustomLevelsPath = Join-Path $LevelsPath 'CustomLevels'
 $CustomLevelInfoFiles = Get-ChildItem $CustomLevelsPath -Recurse -Filter 'info.dat'
 $LevelStats = New-Object 'System.Collections.Generic.List[object]' -ArgumentList $CustomLevelInfoFiles.Length
 $Stopwatch = New-Object System.Diagnostics.Stopwatch
+# for each directory with info.dat (song)
 foreach ($levelInfoFile in $CustomLevelInfoFiles) {
     Write-Verbose "processing $($levelInfoFile.Directory.Name)"
     $Stopwatch.Restart()
@@ -130,16 +132,18 @@ foreach ($levelInfoFile in $CustomLevelInfoFiles) {
         'Mapper' = $levelInfoSrc._levelAuthorName;
         'BPM' = $levelInfoSrc._beatsPerMinute;
         'Envrionment' = $levelInfoSrc._environmentName;
+        '~Duration' = [double]0;
     }
     # setup object to be the same every level
     foreach ($prefix in $DifficultyRankMap) {
-        $levelInfo["$prefix Valid"] = $levelInfo["$prefix Plays"] = $levelInfo["$prefix Rank"] = $levelInfo["$prefix Combo"] = $levelInfo["$prefix Score"] = $levelInfo["$prefix NP10S"] = $levelInfo["$prefix ~Duration"] = $levelInfo["$prefix ~NPS"] = $levelInfo["$prefix Notes"] = ''
+        $levelInfo["$prefix Valid"] = $levelInfo["$prefix Plays"] = $levelInfo["$prefix Rank"] = $levelInfo["$prefix Combo"] = $levelInfo["$prefix Score"] = $levelInfo["$prefix NP10S"] = $levelInfo["$prefix ~NPS"] = $levelInfo["$prefix Notes"] = ''
     }
     Write-Debug "info done at `t$($Stopwatch.ElapsedMilliseconds)"
 
-    $standardMaps = ($levelInfoSrc._difficultyBeatmapSets | Where-Object { $_._beatmapCharacteristicName -eq 'Standard' })
+    # for each characteristic (e.g. standard, one-hand, 90deg, lawless, etc.)
     for ($characteristicIdx = 0; $characteristicIdx -lt $levelInfoSrc._difficultyBeatmapSets.Length; $characteristicIdx++) {
         $characteristicBeatmapSet = $levelInfoSrc._difficultyBeatmapSets[$characteristicIdx]
+        # for each difficulty level on the characteristic
         for ($difficultyIdx = 0; $difficultyIdx -lt $characteristicBeatmapSet._difficultyBeatmaps.Length; $difficultyIdx++) {
             $difficultyInfo = $characteristicBeatmapSet._difficultyBeatmaps[$difficultyIdx]
             $isFinalHashedFile = ($difficultyIdx -eq $characteristicBeatmapSet._difficultyBeatmaps.Length - 1) -and ($characteristicIdx -eq $levelInfoSrc._difficultyBeatmapSets.Length - 1)
@@ -151,20 +155,21 @@ foreach ($levelInfoFile in $CustomLevelInfoFiles) {
                 # read beatmap and calc stats
                 $levelInfo["$prefix Notes"] = $beatmapNotes.Length
                 # TODO for real NPS, song length comes from reading _songFilename. need ffmpeg?
+                # note._time is floating-point beats
                 $firstNoteTime = $beatmapNotes[0]._time
                 $lastNoteTime = $beatmapNotes[$beatmapNotes.Length - 1]._time
-                $notesDuration = $lastNoteTime - $firstNoteTime
-                $levelInfo["$prefix ~NPS"] = [Math]::Round($beatmapNotes.Length / $notesDuration, 2)
-                # TODO only need this once, maybe keep max last and min first note times of all difficulties
-                $levelInfo["$prefix ~Duration"] = [string][Math]::Floor($notesDuration / 60) + ':' + [Math]::Floor($notesDuration % 60)
+                $notesDurationSeconds = ($lastNoteTime - $firstNoteTime) / $levelInfo['BPM'] * 60
+                $levelInfo["$prefix ~NPS"] = [Math]::Round($beatmapNotes.Length / $notesDurationSeconds, 2)
+                $levelInfo['~Duration'] = [Math]::Max($levelInfo['~Duration'], $notesDurationSeconds)
 
                 # highest 10-second NPS
                 # TODO use 2 indexes to look at original array instead of a new one?
                 [double]$highestSoFar = 0
-                $notes = New-Object 'System.Collections.Generic.List[float]'
+                $notes = New-Object 'System.Collections.Generic.List[double]'
+                [double]$tenSecondsInBeats = $levelInfo['BPM'] / 6
                 foreach ($note in $beatmapNotes) {
                     $notes.Add($note._time)
-                    $notes.RemoveAll({param($t) $t -lt $note._time - 10}) >$null
+                    $notes.RemoveAll({param($t) $t -lt $note._time - $tenSecondsInBeats}) >$null
                     $notesNps = $notes.Count
                     if ($notesNps -gt $highestSoFar) {
                         $highestSoFar = $notesNps
@@ -177,14 +182,18 @@ foreach ($levelInfoFile in $CustomLevelInfoFiles) {
         }
     }
     Write-Debug "difficulties done at $($Stopwatch.ElapsedMilliseconds)"
+    # format song duration as longest of all difficulties
+    $levelInfo['~Duration'] = [string][Math]::Floor($levelInfo['~Duration'] / 60) + ':' + [Math]::Floor($levelInfo['~Duration'] % 60)
 
     # read save file for scores and stuff
     $hashStr = [System.BitConverter]::ToString($hasher.Hash) -Replace '-',''
     $levelId = "custom_level_$hashStr"
     $levelInfo['ID'] = $levelId
-    $scores = $PlayerData.levelsStatsData | Where-Object {$_.levelId -iLike $levelId -and $_.beatmapCharacteristicName -eq 'Standard'}
+    # sort because something put lowercase hashes in my save file (SongCore uses uppercase)
+    # TODO sort by valid, then score
+    $scores = $PlayerData.levelsStatsData | Where-Object {$_.levelId -iLike $levelId -and $_.beatmapCharacteristicName -eq 'Standard'} | Sort-Object -Property levelId -CaseSensitive
     foreach ($score in $scores) {
-        $prefix = $DifficultyRankMap[[Math]::Floor($score.difficulty / 2)]
+        $prefix = $DifficultyRankMap[[Math]::Floor($score.difficulty)]
         $levelInfo["$prefix Score"] = $score.highScore
         if ($score.fullCombo) {
             $levelInfo["$prefix Combo"] = 'FC'
@@ -202,8 +211,10 @@ foreach ($levelInfoFile in $CustomLevelInfoFiles) {
     Write-Verbose "processed $levelId"
 }
 
+#region output
 # TODO use -Append to avoid storing all in memory until the end?
 Remove-Item 'stats.csv'
 foreach ($lvl in $LevelStats) {
     Export-Csv -InputObject ([pscustomobject]$lvl) -Append -Path 'stats.csv'
 }
+#endregion
